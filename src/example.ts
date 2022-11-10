f6ef39ebbe4c345307e10ffb791292a5716fcd06
Jacob terrell <jacobit97@hotmail.com>
2022-11-10 10:51:31 -0700

    my stuff

--- src/example.ts
+++ src/example.ts
@@ -31,13 +31,37 @@ dotenv.config();
 
     // test searchAllStatesByBusinessName
     // await searchAllStates();
-    
+
     // Get by name
-    // await getDetails("pizza", 'arizona', true, true, true, null, null, null);
+    // await getDetails("BLOOD HOUND AUTO RECOVERY LLC", 'kentucky', true, true, true, null, null, null);
+    // await getDetails("dp development llc", 'washington', true, false, false, null, null, null);
+    // await getDetails("a.i.w., inc.", 'washington', true, false, false, null, null, null);
+    // await getDetails("salter az holdings llc", 'washington', true, false, false, null, null, null);
+    // await getDetails("John Jacob Jingheimer Schmitt His Name Is My  Name Toooo SO lets do this", 'washington', true, false, false, null, null, null);
+    // await getDetails("wright management services, ll", 'washington', true, false, false, null, null, null);
+    // await getDetails("AUSGANICA USA, LLC", 'washington', true, false, false, null, null, null);
+    // await getDetails("the feels beauty supply llc", 'washington', true, false, false, null, null, null);
+    // await getDetails("customizit design & fab llc", 'washington', true, false, false, null, null, null);
+    // await getDetails("pizza", 'ohio', true, true, false, null, null, null);
 
-    // await getDetailsBySosId('7067424', 'pennsylvania', true, true, true);
+    // getDetails("The Pizza Ham Dogs of Detroit", 'washington', true, false, false, null, null, null);
+    // await getDetails("McCoy McKoy LLC", 'wisconsin', true, false, false, null, null, null);
+    // getDetails("Cashless money", 'washington', true, false, false, null, null, null);
+    // getDetails("Pirates", 'washington', true, false, false, null, null, null);
+    // getDetails("Dauntless moon", 'washington', true, false, false, null, null, null);
 
-    await getDetailsByPersonName('Peter', '', 'alaska', true, false, false, null, null, null);
+    // await getDetails("pizza", 'ohio', true, false, false, null, null, null);
+    // await getDetails("Pasta", 'ohio', true, false, false, null, null, null);
+    // await getDetails("construction", 'ohio', true, false, false, null, null, null);
+    // await getDetails("C.Moore Quality Const", 'ohio', true, false, false, null, null, null);
+    // await getDetails("Skin By Sarah", 'Massachusetts', true, false, false, null, null, null);
+    // await getDetails("American Adult Day Care Inc", 'Massachusetts', true, false, false, null, null, null);
+    await getDetails("Hillmers on main", 'vermont', true, false, false, null, null, null);
+
+
+    // await getDetailsBySosId('203994774', 'massachusetts', true, true, true);
+
+    // await getDetailsByPersonName('Community CPA & Associates,', '', 'california', false, false, false, null, null, null);
 
     // await getListBySearchQuery('pizza tax sean', 'wy', true);
 
@@ -46,10 +70,10 @@ dotenv.config();
 
 async function getDetails(businessName: string, state: string, liveData?: boolean, screenshot?: boolean, uccData?: boolean, street?: string, city?: string, zip?: string) {
     const sosApi = new SosApi(process.env.cobaltIntApiKey, 'dev');
-
+    // const sosApi = new SosApi(process.env.jacobAPIKey, 'dev');
     const details = await sosApi.getBusinessDetails(businessName, state, liveData, screenshot, uccData, street, city, zip);
 
-    console.log('details', details, details?.results?.[0]?.uccData, details?.results?.[0]?.documents);
+    console.log('details', details, details?.results?.[0]?.uccData, details?.results?.[0]?.messages, details?.results?.[0]?.documents);
 }
 
 async function getDetailsBySosId(sosId: string, state: string, liveData?: boolean, screenshot?: boolean, uccData?: boolean, street?: string, city?: string, zip?: string) {
